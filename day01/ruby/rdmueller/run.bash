368b0e0b9101032ded5cccde42ac24ad53560b24
Ralf D. Müller <ralf.d.mueller@gmail.com>
2019-12-01 21:54:39 +0100

    Day1 Star2 - finally (#49)
    * trying to use sdk in dockerfile
    
    * Update .gitpod.dockerfile
    
    * Update .gitpod.dockerfile
    
    * Update .gitpod.dockerfile
    
    * Update .gitpod.dockerfile
    
    * Update .gitpod.dockerfile
    
    * Update .gitpod.dockerfile
    
    * Update .gitpod
    
    * day01 star2 and clean up

---
+++ day01/ruby/rdmueller/run.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+./solution/day1s1.rb &> results/solution_day1s1.out
+./tests/day1s1.rb &> results/testresult_day1s1.out
+./solution/day1s2.rb &> results/solution_day1s2.out
+./tests/day1s2.rb &> results/testresult_day1s2.out

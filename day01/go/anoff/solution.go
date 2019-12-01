08249c6a0095ee9c343e408e449430ae2ac4050e
Andreas Offenhaeuser <offenhaeuser@gmail.com>
2019-12-01 10:04:59 +0100

    :star2: feat: part2

--- day01/go/anoff/solution.go
+++ day01/go/anoff/solution.go
@@ -5,31 +5,41 @@ import (
 	"math"
 )
 
-// tag::part1[]
 func main() {
 	input := readInput("./input.txt")
 	part1(StringSlice2IntSlice(input))
+	part2(StringSlice2IntSlice(input))
 }
 
 func part1(massInventory []int) {
 	var fuelSum int = 0
 	for _, mass := range massInventory {
-		fmt.Println(mass)
-		fuelSum += RequiredFuel(mass)
+		fuelSum += requiredFuel(mass)
 	}
 	fmt.Println("Solution for part1:", fuelSum)
 }
 
-// end::part1[]
-func part2() {
-
+func part2(massInventory []int) {
+	var fuelSum int = 0
+	for _, mass := range massInventory {
+		fuelSum += requiredFuelRecursive(mass)
+	}
+	fmt.Println("Solution for part2:", fuelSum)
 }
 
-// tag::part1[]
-func RequiredFuel(mass int) int {
+func requiredFuel(mass int) int {
 	var massFloat = float64(mass)
 	var fuelFloat = math.Floor(massFloat / 3)
 	return int(fuelFloat) - 2
 }
 
-// end::part1[]
+func requiredFuelRecursive(mass int) int {
+	var massFloat = float64(mass)
+	var fuelFloat = math.Floor(massFloat/3) - 2
+	var fuelInt = int(math.Max(fuelFloat, 0))
+	if fuelInt > 0 {
+		var additionalFuel = requiredFuelRecursive(fuelInt)
+		fuelInt += additionalFuel
+	}
+	return fuelInt
+}
--- day01/go/anoff/solution_test.go
+++ day01/go/anoff/solution_test.go
@@ -12,7 +12,19 @@ func TestRequiredFuel(t *testing.T) {
 	m[1969] = 654
 	m[100756] = 33583
 	for mass, fuel := range m {
-		res := RequiredFuel(mass)
+		res := requiredFuel(mass)
+		assert.Equal(t, fuel, res, "Wrong result for mass=%d", mass)
+	}
+}
+
+func TestRequiredFuelRecursive(t *testing.T) {
+	m := make(map[int]int)
+	m[12] = 2
+	m[14] = 2
+	m[1969] = 966
+	m[100756] = 50346
+	for mass, fuel := range m {
+		res := requiredFuelRecursive(mass)
 		assert.Equal(t, fuel, res, "Wrong result for mass=%d", mass)
 	}
 }

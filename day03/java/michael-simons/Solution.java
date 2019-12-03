d4d1d7551e6accbc21ab66a79dd45f9b5c3a347c
Michael Simons <michael@simons.ac>
2019-12-03 10:06:45 +0100

    Polishing.

--- day03/java/michael-simons/Solution.java
+++ day03/java/michael-simons/Solution.java
@@ -4,11 +4,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
 import java.util.function.Function;
 

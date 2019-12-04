849a5a502e2318e7b3d63776f81b5ee482dbb717
beckel <beckel@inf.ethz.ch>
2019-12-04 07:19:42 +0100

    Solved Day 03 - Star 1

--- day03/python/wiedmann/manhattan.py
+++ day03/python/wiedmann/manhattan.py
@@ -1,18 +1,23 @@
 import numpy as np
 import math
 
+debug = True
+
 def compute(input_1, input_2):
         
     # convert steps into more computable form: [ [x1,y1], [x2,y2], ... ]
     steps_cable_1 = []
     steps_cable_2 = []
+    print("Extract steps from file")
     extract_steps(input_1, steps_cable_1)
     extract_steps(input_2, steps_cable_2)
     
     # put cables on board
-    board = np.zeros([100000, 100000], dtype=int)
-    [max_x_cable_1, max_y_cable_1, min_x_cable_1, min_y_cable_1] = walk_over_board(board, steps_cable_1)
-    [max_x_cable_2, max_y_cable_2, min_x_cable_2, min_y_cable_2] = walk_over_board(board, steps_cable_2)
+    board = np.zeros([100000, 100000, 2], dtype=int)
+    print("Putting cable 1 on board")
+    [max_x_cable_1, max_y_cable_1, min_x_cable_1, min_y_cable_1] = walk_over_board(board, steps_cable_1, 0)
+    print("Putting cable 2 on board")
+    [max_x_cable_2, max_y_cable_2, min_x_cable_2, min_y_cable_2] = walk_over_board(board, steps_cable_2, 1)
 
     x_min = max(min_x_cable_1, min_x_cable_2)
     y_min = max(min_y_cable_1, min_y_cable_2)
@@ -21,14 +26,16 @@ def compute(input_1, input_2):
         
     # for each field except [0,0] check if field is covered by both cables
     smallest_manhattan_distance = 100000000
-    print("x_min", x_min, "x_max", x_max)
-    print("y_min", y_min, "y_max", y_max)
+    if debug:
+        print("Common cable area ranges from x:", x_min, "to", x_max)
+        print("Common cable area ranges from y:", y_min, "to", y_max)
     for i in range(x_min, x_max):
         for j in range(y_min, y_max):
             if (i==0 and j==0):
                 continue
-            if (board[i][j] == 2):
-                print(i," ",j)
+            if (board[i][j][0] == 1 and board[i][j][1]) == 1:
+                if debug:
+                    print(i," ",j)
                 distance = abs(i)+abs(j)
                 smallest_manhattan_distance = min(distance, smallest_manhattan_distance)
     print("Result: " + str(smallest_manhattan_distance))
@@ -47,18 +54,17 @@ def extract_steps(steps_input, steps_output):
         if (char_1 == 'R'):
             [steps_output.append([1,0]) for x in range(0, num_steps)]
             
-def walk_over_board(board, steps):
+def walk_over_board(board, steps, cable_id):
     x = 0
     y = 0
-    max_x = 0
-    max_y = 0
-    min_x = 0
-    min_y = 0
-    board[x,y] += 1
+    max_x = -100000
+    max_y = -100000
+    min_x = 100000
+    min_y = 100000
     for i in range(0, len(steps)):
         x += steps[i][0]
         y += steps[i][1]
-        board[x,y] += 1
+        board[x,y,cable_id] = 1
         max_x = max(x, max_x)
         max_y = max(y, max_y)
         min_x = min(x, min_x)

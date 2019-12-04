68691604ad68cec8def8746c71c4cfdb4be30b57
Micah Halter <micah.halter@gtri.gatech.edu>
2019-12-04 14:05:55 -0500

    Code cleanup

--- day04/haskell/mehalter/answer.hs
+++ day04/haskell/mehalter/answer.hs
@@ -6,13 +6,11 @@ digits = map (read . return) . show
 
 -- check if list is sorted
 isSorted :: Ord a => [a] -> Bool
-isSorted [] = True
-isSorted [x] = True
-isSorted (x:y:xs) = x <= y && isSorted (y:xs)
+isSorted list = all (uncurry (<=)) $ zip list $ tail list
 
 -- check if the number an item appears satisfies a supplied function
 containsN :: Ord a => (Int -> Bool) -> [a] -> Bool
-containsN func list = (<) 0 $ length $ filter (\l -> func $ length l) $ group list
+containsN func = (<) 0 . length . filter (\l -> func $ length l) . group
 
 -- Check if digits are sorted and if there is a digit who's appearance satisfies a function
 valid :: Ord a => (Int -> Bool) -> [a] -> Bool

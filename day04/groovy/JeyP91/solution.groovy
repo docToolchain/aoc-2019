ad531897aa21e7043b1f7ef2745ea6de44edddc8
Julian Arndt <julian@arndt-web.net>
2019-12-05 21:52:34 +0100

    Improve solution of day 4 and add documentation

--- day04/groovy/JeyP91/solution.groovy
+++ day04/groovy/JeyP91/solution.groovy
@@ -4,23 +4,32 @@ List<Integer> validNumbersPart2 = new ArrayList<Integer>()
 for (int number = 235741; number <= 706948; number++) {
 // for (int number = 123666; number <= 123666 ; number++) { 
     List<Integer> positions = new ArrayList<Integer>()
-    positions[5] = (number - (number % 100000)) / 100000
-    positions[4] = (number - positions[5] * 100000 - (number % 10000)) / 10000
-    positions[3] = (number - positions[4] * 10000 - positions[5] * 100000 - (number % 1000)) / 1000
-    positions[2] = (number - positions[3] * 1000 - positions[4] * 10000 - positions[5] * 100000 - (number % 100)) / 100
-    positions[1] = (number - positions[2] * 100 - positions[3] * 1000 - positions[4] * 10000 - positions[5] * 100000 - (number % 10)) / 10
-    positions[0] = (number - positions[1] * 10 - positions[2] * 100 - positions[3] * 1000 - positions[4] * 10000 - positions[5] * 100000)
+
+    // Split up number into single digits
+    // tag::splitUpNumber[]
+    positions[5] = Math.floor(number / 100000)
+    positions[4] = Math.floor(number % 100000 / 10000)
+    positions[3] = Math.floor(number % 10000 / 1000)
+    positions[2] = Math.floor(number % 1000 / 100)
+    positions[1] = Math.floor(number % 100 / 10)
+    positions[0] = number % 10
+    // end::splitUpNumber[]
+
+    // Add number to list of successfully checked numbers if rules are followed
+    // tag::iterateAllNumbers[]
     if(checkDoubleNumber(positions) && checkNeverDecrease(positions)) {
         validNumbersPart1.add(number)
         if(checkIsolatedDoubleNumber(positions)) {
             validNumbersPart2.add(number)
         }
     }
+    // end::iterateAllNumbers[]
 }
 
 println "Solution Part 1: " + validNumbersPart1.size()
 println "Solution Part 2: " + validNumbersPart2.size()
 
+// Check if at least two adjacent digits are the same
 boolean checkDoubleNumber(List<Integer> positions) {
     for(int i = 0; i < 5; i++) {
         if(positions[i] == positions[i+1]) {
@@ -30,15 +39,20 @@ boolean checkDoubleNumber(List<Integer> positions) {
     return false
 }
 
+// Check if going from left to right, the digits never decrease
 boolean checkNeverDecrease(List<Integer> positions) {
+    
+    // tag::checkNeverDecrease[]
     for(int i = 0; i < 5; i++) {
         if(positions[i] < positions [i+1]) {
             return false
         }
     }
     return true
+    // end::checkNeverDecrease[]
 }
 
+// Check if the two adjacent matching digits are not part of a larger group of matching digits
 boolean checkIsolatedDoubleNumber(List<Integer> positions) {
     for(int i = 1; i < 4; i++) {
         if(positions[i] == positions[i+1] &&

199970bd6ac85ff590aed860871c0512e2c6f238
beckel <beckel@inf.ethz.ch>
2019-12-07 12:17:08 +0100

    Day 07 Part 2

--- day07/python/beckel/amplifier_sequence.py
+++ day07/python/beckel/amplifier_sequence.py
@@ -9,3 +9,27 @@ def run_standalone(program, phase_configuration):
         in_val = out_val
 
     return out_val
+
+def run_feedback(program, phase_configuration):
+
+    in_val = 0
+    num_amplifiers = len(phase_configuration)
+    
+    # initialize amplifiers
+    amplifiers = []
+    for i in phase_configuration:
+        amplifiers.append(Computer(program.copy(), [i]))
+    
+    # start first amplifier
+    amplifiers[0].provide_input(in_val)
+
+    # run amplifier and connect output to next amplifier (if not halted)
+    i = 0
+    while(True):
+        amplifiers[i].run()
+        output = amplifiers[i].get_last_output()
+        i = (i+1) % num_amplifiers
+        if (amplifiers[i].has_terminated() == False):
+            amplifiers[i].provide_input(output)
+        else:
+            return output
--- day07/python/beckel/test.py
+++ day07/python/beckel/test.py
@@ -16,8 +16,27 @@ signal_output_3 = 65210
 result_1 = amplifier_sequence.run_standalone(program_1, phase_setting_1)
 assert(result_1 == signal_output_1)
 
-result_1 = amplifier_sequence.run_standalone(program_2, phase_setting_2)
-assert(result_1 == signal_output_2)
+result_2 = amplifier_sequence.run_standalone(program_2, phase_setting_2)
+assert(result_2 == signal_output_2)
 
-result_1 = amplifier_sequence.run_standalone(program_3, phase_setting_3)
-assert(result_1 == signal_output_3)
+result_3 = amplifier_sequence.run_standalone(program_3, phase_setting_3)
+assert(result_3 == signal_output_3)
+
+print("Tests for standalone amplifiers successful.")
+
+program_1 = [3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5]
+program_2 = [3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10]
+
+phase_setting_1 = [9,8,7,6,5]
+phase_setting_2 = [9,7,8,5,6]
+
+signal_output_1 = 139629729
+signal_output_2 = 18216
+
+result_1 = amplifier_sequence.run_feedback(program_1, phase_setting_1)
+assert(result_1 == signal_output_1)
+
+result_2 = amplifier_sequence.run_feedback(program_2, phase_setting_2)
+assert(result_2 == signal_output_2)
+
+print("Tests for feedback amplifiers successful.")

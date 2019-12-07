c651a757673151589015b553743ff756c9e90e6e
Robert Hansel <robert@fam-hansel.de>
2019-12-07 14:21:01 +0100

    still no luck solving the issue

--- day05/python/haro87/enterprise.py
+++ day05/python/haro87/enterprise.py
@@ -1,7 +1,136 @@
-class JohnDoe:
+import logging
+from enum import Enum
+
+logging.basicConfig(level=logging.INFO)
+
+class Instruction(Enum):
+    ADD = 1
+    MULTIPLY = 2
+    STORE = 3
+    OUTPUT = 4
+
+class Mode(Enum):
+    POSITION = 0
+    IMMEDIATE = 1
+
+class Command:
+    inst = 0
+    modes = [].copy()
+    def __init__(self, opcode):
+        self.inst = opcode // 10**0 % 10
+        del self.modes[:]
+        self.modes.append(opcode // 10**2 % 10)
+        self.modes.append(opcode // 10**3 % 10)
+        self.modes.append(opcode // 10**4 % 10)
+
+
+    def instruction(self):
+        return self.inst
+
+    def parameter_modes(self):
+        return self.modes
+
+class MontgomeryScott:
 
     def __init__(self):
         pass
 
-    def do(self):
-        pass
+    def _add(self, one, two):
+        return one + two
+
+    def _multiply(self, one, two):
+        return one * two
+
+    def _store(self, value, add, program):
+        if add < len(program):
+            program[add] = value
+        else:
+            logging.error("ERROR -> Address: {0} out of bounds".format(add))
+
+    def _output(self, add, program):
+        if add < len(program):
+            return program[add]
+        else:
+            logging.error("ERROR -> Address: {0} out of bounds".format(add))
+            return None
+
+    def _get_parameters(self, count, command, program):
+        params = []
+        pos = count + 1
+        for mode in command.parameter_modes():
+            if mode == Mode.IMMEDIATE.value:
+                if pos < len(program):
+                    params.append(program[pos])
+                else:
+                    logging.error("ERROR -> Address: {0} out of bounds".format(pos))
+            if mode == Mode.POSITION.value:
+                if pos < len(program):
+                    add = program[pos]
+                    if len(params) == 2:
+                        if  add < len(program):
+                            params.append(add)
+                        else:
+                            logging.error("ERROR -> Address: {0} out of bounds".format(add))
+                    else:    
+                        if  add < len(program):
+                            params.append(program[add])
+                        else:
+                            logging.error("ERROR -> Address: {0} out of bounds".format(add))
+                else:
+                    logging.error("ERROR -> Address: {0} out of bounds".format(pos))
+            pos = pos + 1
+
+        if command.instruction() > 2 and command.instruction() <= 4:
+             params = [params[0]]
+        logging.info("INFO -> Collected paramters: {0}".format(params))
+
+        return params
+
+    def translate(self, code):
+        
+        trans = str(code).split(',')
+        trans = list(map(int, trans))
+        return trans
+        
+
+    def execute(self, commands):
+        count = 0
+        while count <= len(commands):
+            c =  commands[count]
+            if c == 99:
+                break
+            command = Command(c)
+            if command.instruction() < 1 or command.instruction() > 4:
+                count = count + 1
+                continue
+            logging.info("INFO -> Instruction: {0}".format(command.instruction()))
+            params = self._get_parameters(count, command, commands)
+            logging.info("INFO -> Parameters: {0}".format(params))
+            if command.instruction() == Instruction.ADD.value:
+                if len(params) < 3:
+                    logging.error("ERROR -> Not enough paramters for ADD at position {0}".format(count))
+                    continue
+                self._store(self._add(params[0], params[1]), params[2], commands)
+                count = count + 4
+            elif command.instruction() == Instruction.MULTIPLY.value:
+                if len(params) < 3:
+                    logging.error("ERROR -> Not enough paramters for MULTIPLY at position {0}".format(count))
+                    continue
+                self._store(self._multiply(params[0], params[1]), params[2], commands)
+                count = count + 4
+            elif command.instruction() == Instruction.STORE.value:
+                if len(params) < 1:
+                    logging.error("ERROR -> Not enough paramters for STORE at position {0}".format(count))
+                    continue
+                self._store(1, params[0], commands)
+                count = count +2
+            elif command.instruction() == Instruction.OUTPUT.value:
+                if len(params) < 1:
+                    logging.error("ERROR -> Not enough paramters for OUTPUT at position {0}".format(count))
+                    continue
+                print("-->OUTPUT: {0}".format(self._output(params[0], commands)))
+                count = count + 2
+            else:
+                count = count + 1
+        return commands
+
--- day05/python/haro87/enterprise_test.py
+++ day05/python/haro87/enterprise_test.py
@@ -1,9 +1,23 @@
 import pytest
 from unittest import TestCase
-from enterprise import JohnDoe as JD
+from enterprise import MontgomeryScott as Scotty
 
 class Test_Enterprise(TestCase):
     def test_translate(self):
-        pass
+        scotty = Scotty()
+        res = scotty.translate('3,0,4,0,99')
+        self.assertEqual(res, [3, 0, 4, 0, 99])
 
+    def test_execute_one(self):
+        scotty = Scotty()
+        trans = scotty.translate('3,0,4,0,99')
+        res = scotty.execute(trans)
+        self.assertEqual(res, [3, 0, 4, 1, 99])
+
+    def test_execute_two(self):
+        scotty = Scotty()
+        trans = scotty.translate('1002,4,3,4,33')
+        res = scotty.execute(trans)
+        self.assertEqual(res, [1002,4,3,4,99])
+    
     

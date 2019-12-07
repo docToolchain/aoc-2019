9ac54ce919f6c4902cc92307ea7281e0823461bc
Robert Hansel <robert@fam-hansel.de>
2019-12-07 20:29:34 +0100

    still working on part two

--- day05/python/haro87/enterprise.py
+++ day05/python/haro87/enterprise.py
@@ -8,6 +8,10 @@ class Instruction(Enum):
     MULTIPLY = 2
     STORE = 3
     OUTPUT = 4
+    JUMP_IF_TRUE = 5
+    JUMP_IF_FALSE = 6
+    LESS_THAN = 7
+    EQUALS = 8
 
 class Mode(Enum):
     POSITION = 0
@@ -32,8 +36,8 @@ class Command:
 
 class MontgomeryScott:
 
-    def __init__(self):
-        pass
+    def __init__(self, input):
+        self.input = input
 
     def _add(self, one, two):
         return one + two
@@ -58,6 +62,8 @@ class MontgomeryScott:
         params = []
         pos = count + 1
         for mode in command.parameter_modes():
+            if command.instruction() > 4 and command.instruction() < 7 and len(params) == 2:
+                break
             if mode == Mode.IMMEDIATE.value:
                 if pos < len(program):
                     params.append(program[pos])
@@ -94,17 +100,18 @@ class MontgomeryScott:
         
 
     def execute(self, commands):
+        output = []
         count = 0
         while count <= len(commands):
             c =  commands[count]
             if c == 99:
                 break
             command = Command(c)
-            if command.instruction() < 1 or command.instruction() > 4:
+            if command.instruction() < 1 or command.instruction() > 8:
                 count = count + 1
                 continue
             logging.info("INFO -> Instruction: {0}".format(command.instruction()))
-            if command.instruction() > 0 and command.instruction() < 3:
+            if (command.instruction() > 0 and command.instruction() < 3) or command.instruction() > 4:
                 params = self._get_parameters(count, command, commands)
                 logging.info("INFO -> Parameters: {0}".format(params))
             if command.instruction() == Instruction.ADD.value:
@@ -120,12 +127,32 @@ class MontgomeryScott:
                 self._store(self._multiply(params[0], params[1]), params[2], commands)
                 count = count + 4
             elif command.instruction() == Instruction.STORE.value:
-                self._store(1, commands[count + 1], commands)
+                self._store(self.input, commands[count + 1], commands)
                 count = count +2
             elif command.instruction() == Instruction.OUTPUT.value:
-                print("-->OUTPUT: {0}".format(self._output(commands[count + 1], commands)))
+                out = self._output(commands[count + 1], commands)
+                output.append(out)
+                print("-->OUTPUT: {0}".format(out))
                 count = count + 2
+            elif command.instruction() == Instruction.JUMP_IF_TRUE.value:
+                if params[0] != 0:
+                    count = params[1]
+                else:
+                    count = count + 3
+            elif command.instruction() == Instruction.JUMP_IF_FALSE.value:
+                if params[0] == 0:
+                    count = params[1]
+                else:
+                    count = count + 3
+            elif command.instruction() == Instruction.LESS_THAN.value:
+                if params[0] < params [1]:
+                    self._store(1, params[2], commands)
+                count = count + 4
+            elif command.instruction() == Instruction.EQUALS.value:
+                if params[0] == params[1]:
+                    self._store(1, params[2], commands)
+                count = count + 4
             else:
                 count = count + 1
-        return commands
+        return output
 

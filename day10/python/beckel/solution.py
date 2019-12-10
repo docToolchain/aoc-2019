f9803989fa037b3e046cd9106e4d02957f8cf1a0
beckel <beckel@inf.ethz.ch>
2019-12-10 22:37:37 +0100

    Day 10 - Stars 1 and 2

--- day10/python/beckel/solution.py
+++ day10/python/beckel/solution.py
@@ -1,4 +1,6 @@
 import numpy as np
+import math
+from collections import defaultdict
 
 debug = False
 field = []
@@ -10,21 +12,21 @@ with open("input.txt") as fd:
     x = np.transpose(x)
 
     # count visible asteroids
-    max_visible_asteroids = { 'x': 0, 'y': 0, 'num': 0 }
+    max_visible_asteroids = { 'x': 0, 'y': 0, 'num': 0, 'directions': defaultdict(list) }
     it = np.nditer(x, flags=['multi_index'])
     while not it.finished:
+        directions = defaultdict(list)
         i = it.multi_index[0]
         j = it.multi_index[1]
         if debug:
             print("Checking field",i,",",j)
 
-        directions = dict()
-
         # do not proceed if there is no astroid
         if x[i,j] == 0:
             it.iternext()
             continue
-
+        
+        # identify visible targets
         it_target = np.nditer(x, flags=['multi_index'])
         while not it_target.finished:
             i_target = it_target.multi_index[0]
@@ -40,46 +42,35 @@ with open("input.txt") as fd:
                 it_target.iternext()
                 continue
 
-            # store direction
-            dir = np.arctan2(j_target-j, i_target-i)
-            directions[dir] = 1
+            # store direction and distance
+            dir = np.arctan2(i_target-i, j_target-j)
+            distance = math.sqrt((j_target-j)**2 + (i_target-i)**2)
+            directions[dir].append({'x': i_target, 'y': j_target, 'distance': distance})
 
             it_target.iternext()
 
-
-            # check if space between two asteroids at [i,j] and [i_target,j_target] is free
-            # x_dir = 1 if (i_target - i) >= 0 else -1
-            # y_dir = 1 if (j_target - j) >= 0 else -1
-            # visibility_blocked = False
-            # dy = 0
-
-            # # all directions except vertical
-            # for dx in range(i + x_dir, i_target, x_dir):
-            #     dy += y_dir * abs(j_target-j) / abs(i_target-i)
-            #     i_check = dx
-            #     j_check = j + dy
-            #     if j_check.is_integer() and x[i_check,int(j_check)] == 1:
-            #         visibility_blocked = True
-
-            # # y steps if vertical direction
-            # if i_target == i:
-            #     for j_check in range(j + y_dir, j_target, y_dir):
-            #         if x[i,j_check] == 1:
-            #             visibility_blocked = True
-
-            # if visibility_blocked == False:
-            #     visible_asteroids_from_here += 1
-            # it_target.iternext()
-            # continue
-
         visible_asteroids_from_here = len(directions.keys())
-        if (debug):
-            print("Visible asteroids",visible_asteroids_from_here)
         if visible_asteroids_from_here > max_visible_asteroids['num']:
             max_visible_asteroids['num'] = visible_asteroids_from_here
             max_visible_asteroids['x'] = i
             max_visible_asteroids['y'] = j
+            max_visible_asteroids['directions'] = directions
         
         it.iternext()
+    
+    print("Part 1: Max. visible asteroids:",max_visible_asteroids['num']," - from (",max_visible_asteroids['x'],",",max_visible_asteroids['y'],")")
 
-    print("Max. visible asteroids:",max_visible_asteroids)
+    # Star 2: Shoot asteroids with laster
+    dir_from_base = max_visible_asteroids['directions']
+    hit_number = 0
+    while (len(dir_from_base) > 0):
+        for direction in sorted(dir_from_base.keys(), reverse=True):
+            asteroids_in_line = dir_from_base[direction]
+            list.sort(asteroids_in_line, key=lambda x:x['distance'])
+            closest = asteroids_in_line.pop(0)
+            if not asteroids_in_line:
+                del dir_from_base[direction]
+            hit_number += 1
+            print("Hit no.",hit_number,": Asteroid at",closest['x'],closest['y'])
+
+        
